3dbcd672203b2e97c570c256adcac2b3b555934f
saycguhj <js1jsoshsis@hotmail.com>
2024-05-06 04:02:27 +0700

    demo project commit

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-function countBits(num) {
-  const result = new Array(num + 1).fill(0);
-  for (let i = 1; i <= num; i++) {
-    result[i] = result[i >> 1] + (i & 1);
-  }
-  return result;
-}
+const initial = (arr) => arr.slice(0, -1);
